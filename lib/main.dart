f00158256246f8d6f30e6eec698c5a1071da3c0c
ShiwamPaudel <106426216+ShiwamPaudel@users.noreply.github.com>
2024-04-13 18:24:45 +0545

    added necessary packages -config commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,140 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:tassist/core/models/inactivecustomer.dart';
+import 'package:tassist/core/models/payables.dart';
+import 'package:tassist/core/models/receivables.dart';
+import 'package:tassist/core/models/stockitem.dart';
+import 'package:tassist/core/models/vouchers.dart';
+import 'package:tassist/core/services/auth.dart';
+import 'package:tassist/core/services/database.dart';
+import 'package:tassist/core/services/inactivecustomerservice.dart';
+import 'package:tassist/core/services/payablesservice.dart';
+import 'package:tassist/core/services/receivablesservice.dart';
+import 'package:tassist/core/services/stockservice.dart';
+import 'package:tassist/core/services/vouchers.dart';
+import 'package:tassist/route_generator.dart';
+import 'package:tassist/theme/texts.dart';
+import 'core/models/company.dart';
+import 'core/models/ledger.dart';
+import 'core/services/companyservice.dart';
+import 'core/services/ledgerservice.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Accounting_pos_Project',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    const String _title = "TallyAssist";
+
+    return MultiProvider(providers: [
+      StreamProvider<FirebaseUser>.value(
+        value: AuthService().user,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    ], child: TopWidget(title: _title));
+  }
+}
+
+class TopWidget extends StatefulWidget {
+  const TopWidget({
+    Key key,
+    @required String title,
+  })  : _title = title,
+        super(key: key);
+
+  final String _title;
+
+  @override
+  _TopWidgetState createState() => _TopWidgetState();
+}
+
+class _TopWidgetState extends State<TopWidget> {
+
+  final FirebaseMessaging _fcm = FirebaseMessaging();
+
+  @override
+  void initState() {
+    super.initState();
+    _saveDeviceToken();
+
+
+    _fcm.configure(
+      onMessage: (Map<String, dynamic> message) async {
+        print("onMessage: $message");
+        showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+            content: ListTile(
+              title: Text(message['notification']['title']),
+              subtitle: Text(message['notification']['body']),
+            ),
+            actions: <Widget>[
+              FlatButton(
+                color: Colors.amber,
+                child: Text('Ok'),
+                onPressed: () => Navigator.of(context).pop(),
+              ),
+            ],
+          ),
+        );
+      },
+      onLaunch: (Map<String, dynamic> message) async {
+        print("onLaunch: $message");
+      },
+      onResume: (Map<String, dynamic> message) async {
+        print("onResume: $message");
+      },
     );
   }
-}
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+  /// Get the token, save it to the database for current user
+  _saveDeviceToken() async {
+    await _fcm.getToken();
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+    final user = Provider.of<FirebaseUser>(context);
+    return MultiProvider(
+      providers: [
+        StreamProvider<List<ReceivablesItem>>.value(
+          value: ReceivablesItemService(uid: user?.uid).accountsReceivableData,
+        ),
+        // LEDGER/PARTY DATA
+        StreamProvider<List<LedgerItem>>.value(
+            value: LedgerItemService(uid: user?.uid).ledgerItemData),
+        StreamProvider<List<StockItem>>.value(
+            value: StockItemService(uid: user?.uid).stockItemsData),
+        StreamProvider<List<PayablesItem>>.value(
+            value: PayablesItemService(uid: user?.uid).accountsPayablesData),
+        StreamProvider<List<InactiveCustomer>>.value(
+            value:
+            InactiveCustomerService(uid: user?.uid).inactiveCustomerData),
+        StreamProvider<List<Voucher>>.value(
+            value: VoucherService(uid: user?.uid).voucherData),
+        StreamProvider<Company>.value(
+            value: CompanyService(uid: user?.uid).companyData),
+      ],
+      child: MaterialApp(
+        initialRoute: '/',
+        onGenerateRoute: RouteGenerator.generateRoute,
+        title: widget._title,
+        theme: ThemeData(
+          appBarTheme: AppBarTheme(
+            textTheme: TextTheme(headline6: primaryAppBarTitle),
+          ),
+          textTheme: TextTheme(
+              headline6: secondaryListTitle,
+              subtitle1: secondaryCategoryDesc,
+              bodyText1: secondaryListDisc,
+              bodyText2: secondaryListTitle2),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
